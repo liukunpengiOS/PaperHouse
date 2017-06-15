fa2296e8e5c40bf6b4ab7ddc6be68f2f432dba30
liukunpengiOS <1169405067@qq.com>
2017-06-15 16:30:14 +0800

    添加网络库文件

---
+++ PaperHouse/PHAnnotations.swift
@@ -0,0 +1,24 @@
+//
+//  PHAnnotations.swift
+//  PaperHouse
+//
+//  Created by DRex on 2017/6/15.
+//  Copyright © 2017年 liukunpeng. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class PHAnnotations {
+    
+    init() {
+        
+    }
+    
+    //析构函数
+    deinit {
+        
+    }
+    
+    
+}
